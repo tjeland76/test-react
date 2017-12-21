bdaaef330668fd0ad19a7400bbf58797779077d7
Trevor Eland <tjeland76@gmail.com>
2017-12-21 15:21:43 +0000

    Update

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -9,7 +9,7 @@ import newsData from '../src/services/newsData.js';
 import clone from 'clone';
 /* eslint-disable no-console */
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 const app = express();
 const compiler = webpack(config);
 
@@ -21,53 +21,53 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('/newsfeed', function(req, res){
-    
+
     const startIndex = req.query.index,
         pageSize = req.query.page_size,
         newsCount = newsData.newsData.length;
     let moreItems = true;
-    
+
     if (startIndex) {
         const newsArrayCopy = clone(newsData.newsData);
         let newsArray = newsArrayCopy.reverse();
         let endNewsItemIndex = parseInt(startIndex) + parseInt(pageSize);
-        
+
         if (endNewsItemIndex > newsCount) {
             endNewsItemIndex = newsCount;
             moreItems = false;
         }
-        
+
         const newsSegment = newsArray.slice(parseInt(startIndex), endNewsItemIndex);
-        
+
         const newsResponse = {
           newsData: newsSegment,
           index: endNewsItemIndex,
           moreItems: moreItems
         };
-        
+
         res.send(newsResponse);
     } else {
         res.send(newsData);
     }
-        
+
 });
 
 app.get('/newsitem', function(req, res){
-    
+
     const newsId = req.query.id;
     let newsItem = null;
-    
+
     if (newsId) {
         const newsArrayCopy = clone(newsData.newsData);
-        newsItem = newsArrayCopy.find(x => x.id === parseInt(newsId));    
-    } 
-    
-    const newsResponse = {      
+        newsItem = newsArrayCopy.find(x => x.id === parseInt(newsId));
+    }
+
+    const newsResponse = {
         newsData: newsItem
     };
-        
+
     res.send(newsResponse);
-        
+
 });
 
 app.post('/send-contact', (req, res) => {
@@ -80,13 +80,13 @@ app.post('/send-contact', (req, res) => {
     console.log(`Failed to send the message "${'message'}" from <${'name'}> ${'email'} with the error ${error && error.message}`);
     res.redirect('/#error');
   });
-    
+
 });
 
 app.get('*', function(req, res) {
   res.sendFile(path.join( __dirname, '../src/index.html'));
 });
-    
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
